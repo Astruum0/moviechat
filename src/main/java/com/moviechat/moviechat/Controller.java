047e99399524db11dc0040c60c98771de93c599a
Astruum0 <arthurvella33@gmail.com>
2023-02-16 14:34:59 +0100

    switch response

--- src/main/java/com/moviechat/moviechat/Controller.java
+++ src/main/java/com/moviechat/moviechat/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class Controller {
@@ -16,19 +17,41 @@ public class Controller {
 
     @PostMapping("/chat")
     WebHookResponse Chat(@RequestBody WebHookRequest request) {
-        return new WebHookResponse()
-                .setFulfillmentMessages(List.of(
-                        new Message()
-                                .setPlatform("ACTIONS_ON_GOOGLE")
-                                .setSimpleResponses(
-                                        new SimpleResponses()
-                                                .setSimpleResponses(
-                                                        List.of(
-                                                                new SimpleResponse().setTextToSpeech(
-                                                                        request.toString()
-                                                                )
-                                                        )))
-                ));
+        Map<String, String> parameters = request.queryResult.parameters;
+
+        WebHookResponse response = new WebHookResponse();
+
+        String filmType = parameters.get("type");
+        if (filmType == null || filmType == "") {
+            response.setFulfillmentMessages(List.of(
+                    new Message()
+                            .setPlatform("ACTIONS_ON_GOOGLE")
+                            .setSimpleResponses(
+                                    new SimpleResponses()
+                                            .setSimpleResponses(
+                                                    List.of(
+                                                            new SimpleResponse().setTextToSpeech(
+                                                                    "Quel genre de film voulez vous voir"
+                                                            )
+                                                    )))
+            ));
+        } else {
+            response.setFulfillmentMessages(List.of(
+                    new Message()
+                            .setPlatform("ACTIONS_ON_GOOGLE")
+                            .setSimpleResponses(
+                                    new SimpleResponses()
+                                            .setSimpleResponses(
+                                                    List.of(
+                                                            new SimpleResponse().setTextToSpeech(
+                                                                    String.format("Voici les films du genre %s qui pourraient vous intéresser", filmType)
+                                                            )
+                                                    )))
+            ));
+        }
+
+        return response;
+
     }
 
 }
--- src/main/java/com/moviechat/moviechat/WebHookRequest.java
+++ src/main/java/com/moviechat/moviechat/WebHookRequest.java
@@ -1,11 +1,15 @@
 package com.moviechat.moviechat;
 
+import lombok.Data;
+
 import java.util.Map;
 
+@Data
 public class WebHookRequest {
     QueryResult queryResult;
 }
 
+@Data
 class QueryResult {
     String action;
     Map<String, String> parameters;
