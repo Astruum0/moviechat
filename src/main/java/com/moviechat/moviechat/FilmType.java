40ac4a8d9398d6032e011dd2b33cf616bebb2e8d
Astruum0 <arthurvella33@gmail.com>
2023-02-23 14:14:57 +0100

    add filmtypes

--- src/main/java/com/moviechat/moviechat/FilmType.java
+++ src/main/java/com/moviechat/moviechat/FilmType.java
@@ -10,7 +10,16 @@ public class FilmType {
     String name;
     public static List<FilmType> all = List.of(
             new FilmType().setId(28).setName("Action"),
+            new FilmType().setId(12).setName("Aventure"),
+            new FilmType().setId(16).setName("Animation"),
+            new FilmType().setId(35).setName("Comédie"),
+            new FilmType().setId(80).setName("Crime"),
+            new FilmType().setId(99).setName("Documentaire"),
             new FilmType().setId(14).setName("Fantastique"),
             new FilmType().setId(878).setName("Science-Fiction"),
+            new FilmType().setId(27).setName("Horreur"),
+            new FilmType().setId(10752).setName("Guerre"),
             new FilmType().setId(18).setName("Drame"));
+
+
 }
